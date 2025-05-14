cd6b75f0b8da76ffbfb8fe091e704c5788430214
Amir <amiralziyadi@gmail.com>
2025-05-14 11:51:21 +0200

    Enhance Home page layout with new icons and additional text content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,16 @@ import React, { Fragment } from 'react'
 import bigPicture from "../assets/TitleHighlight.png";
 import smallPicture1 from "../assets/LegoHighlight.png";
 import smallPicture2 from "../assets/GameHighlight.png";
+import gameIcon from "../assets/Game-Icon.png";
+import dollIcon from "../assets/docka-icon.png";
+import toyIcon from "../assets/leksak-icon.png"
+import puzzleIcon from "../assets/pussel-icon.png";
 import './Home.css'
 
 function Home() {
   return (
     <Fragment>
+
       <div className="page-column">
           <img src={bigPicture} alt="Title highlight" className="bigPicture" />
           <div className="smallPics">
@@ -15,10 +20,40 @@ function Home() {
           </div>
       </div>
       <div className='text-content'>
-        <h1>Skandinaviens störska leksasbutik</h1>
-        <h2>Används av över 270 000 svenska kunder</h2>
-        <p>Hitta din leksak</p>
+          <h1>Skandinaviens störska leksasbutik</h1>
+          <h2>Används av över 270 000 svenska kunder</h2>
+          <p>Hitta din leksak</p>
       </div>
+
+      <div>
+        <div className='icon-container'>
+          <div>
+            <img src={gameIcon} alt="Game Icon" className='icon' />
+            <p>TV-Spel</p>
+          </div>
+          <div>
+            <img src={dollIcon} alt="Doll Icon" className='icon' />
+            <p>Dockor</p>
+          </div>
+          <div>
+            <img src={toyIcon} alt="Toy Icon" className='icon' />
+            <p>Leksaker</p>
+          </div>
+          <div>
+            <img src={puzzleIcon} alt="Game Icon" className='icon' />
+            <p>Pyssel</p>
+          </div>
+        </div>
+      </div>
+
+      <div className='smalltext-content'>
+        <h2>Brett utbud - låga priser</h2>
+      </div>
+
+
+
+      
+
     </Fragment>
   )
 }
