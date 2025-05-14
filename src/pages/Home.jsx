efa8b14f5c8ef874e71abc4b02af26767ad7d546
Amir <amiralziyadi@gmail.com>
2025-05-14 11:21:06 +0200

    Adding content inside home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import bigPicture from "../assets/TitleHighlight.png";
+import smallPicture1 from "../assets/LegoHighlight.png";
+import smallPicture2 from "../assets/GameHighlight.png";
+import './Home.css'
 
 function Home() {
   return (
-    <div>
-      
+    <div className="page-column">
+        <img src={bigPicture} alt="Title highlight" />
+        <div className="smallPics">
+            <img src={smallPicture1} alt="Lego highlight" />
+            <img src={smallPicture2} alt="Game highlight" />
+        </div>
     </div>
   )
 }
