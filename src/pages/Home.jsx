8cddcbd200457f0e209cf25c13ebd9bd41023b9a
Amir <amiralziyadi@gmail.com>
2025-05-14 11:30:16 +0200

    Add new icons and enhance Home page layout with text content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import bigPicture from "../assets/TitleHighlight.png";
 import smallPicture1 from "../assets/LegoHighlight.png";
 import smallPicture2 from "../assets/GameHighlight.png";
@@ -6,13 +6,20 @@ import './Home.css'
 
 function Home() {
   return (
-    <div className="page-column">
-        <img src={bigPicture} alt="Title highlight" />
-        <div className="smallPics">
-            <img src={smallPicture1} alt="Lego highlight" />
-            <img src={smallPicture2} alt="Game highlight" />
-        </div>
-    </div>
+    <Fragment>
+      <div className="page-column">
+          <img src={bigPicture} alt="Title highlight" className="bigPicture" />
+          <div className="smallPics">
+              <img src={smallPicture1} alt="Lego highlight" />
+              <img src={smallPicture2} alt="Game highlight" />
+          </div>
+      </div>
+      <div className='text-content'>
+        <h1>Skandinaviens störska leksasbutik</h1>
+        <h2>Används av över 270 000 svenska kunder</h2>
+        <p>Hitta din leksak</p>
+      </div>
+    </Fragment>
   )
 }
 
