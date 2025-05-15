0f2cafe1fe6a35615bd1b9a8d1a65e8773d95fb2
Amir <amiralziyadi@gmail.com>
2025-05-16 01:00:03 +0200

    Refactor App component for improved structure; add 404 catch-all route and update copyright display. Adjust Products and Collection CSS for better layout and responsiveness.

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,42 @@
-import React from 'react'
-import {Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Collection from './pages/Collection'
-import Cart from './pages/Cart'
-import Product from './pages/Product'
-import PlaceOrder from './pages/PlaceOrder'
-import Orders from './pages/Orders'
-import Navbar from './components/Navbar'
-import './App.css'
-import Footer from './components/Footer'
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Collection from './pages/Collection';
+import Cart from './pages/Cart';
+import Product from './pages/Product';
+import PlaceOrder from './pages/PlaceOrder';
+import Orders from './pages/Orders';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import './App.css';
 
 const App = () => {
+  const currentYear = new Date().getFullYear();
+  
   return (
-
     <div className='app-container'>
-
-      <Navbar/>
+      <Navbar />
 
       <main>
         <Routes>
-            <Route path ="/" element={<Home/>} />
-            <Route path ="/collection" element={<Collection/>} />
-            <Route path ="/cart" element={<Cart/>} />
-            <Route path ="/product/:productId" element={<Product/>} />
-            <Route path='/placeorder' element={<PlaceOrder/>} />
-            <Route path='/orders' element={<Orders/>} />
+            <Route path="/" element={<Home />} />
+            <Route path="/collection" element={<Collection />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/product/:productId" element={<Product />} />
+            <Route path="/placeorder" element={<PlaceOrder />} />
+            <Route path="/orders" element={<Orders />} />
+            {/* Add a catch-all route for 404 handling */}
+            <Route path="*" element={<div className="not-found">Sidan kunde inte hittas</div>} />
         </Routes>
       </main>
 
-      <footer>
-        <div className='footer-container'>
-          <Footer/>
-        </div>
-        <div className='copyright'>
-          {/* Copyright information */}
-        </div>
-      </footer>
-
+      <Footer />
+      
+      <div className='copyright'>
+        <p>&copy; {currentYear} Leksaksbutiken. Alla rättigheter förbehållna.</p>
+      </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
